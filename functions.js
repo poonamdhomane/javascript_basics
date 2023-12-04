dbc9b58a9a70ff5cd3e6558b2faad9d6cad32595
Poonam Dhomane <poonamdhomane89@gmail.com>
2023-12-04 05:13:36 +0000

    Functions basic

---
+++ functions.js
@@ -0,0 +1,23 @@
+function add( num1, num2, ...num3) { // function declaration - default params
+    return num3; 
+}
+
+
+// let result =  add(3,4);   // function call
+// console.log("final result", result)
+// add(10,20);
+
+
+// let result = add(10)
+// console.log(result)
+
+
+let result =  add(10,20,30,40)
+console.log(result);
+
+
+
+
+
+
+
